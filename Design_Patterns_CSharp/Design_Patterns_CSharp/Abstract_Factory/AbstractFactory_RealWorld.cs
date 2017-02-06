9aebe946b551df42109e3187023917808ce57471
adjutant-xx <matt.paauw@gmail.com>
2017-02-06 15:53:20 -0800

    Updated Abstract Factory code classes with commented-out client code.

--- Design_Patterns_CSharp/Design_Patterns_CSharp/Abstract_Factory/AbstractFactory_RealWorld.cs
+++ Design_Patterns_CSharp/Design_Patterns_CSharp/Abstract_Factory/AbstractFactory_RealWorld.cs
@@ -6,6 +6,32 @@ using System.Threading.Tasks;
 
 namespace Design_Patterns_CSharp.Abstract_Factory
 {
+    ///// <summary>
+    ///// MainApp startup class for Real-World
+    ///// Abstract Factory Design Pattern.
+    ///// </summary>
+    //class MainApp
+    //{
+    //    /// <summary>
+    //    /// Entry point into console application.
+    //    /// </summary>
+    //    public static void Main()
+    //    {
+    //        // Create and run the African animal world
+    //        ContinentFactory africa = new AfricaFactory();
+    //        AnimalWorld world = new AnimalWorld(africa);
+    //        world.RunFoodChain();
+
+    //        // Create and run the American animal world
+    //        ContinentFactory america = new AmericaFactory();
+    //        world = new AnimalWorld(america);
+    //        world.RunFoodChain();
+
+    //        // Wait for user input
+    //        Console.ReadKey();
+    //    }
+    //}
+
     /// <summary>
     /// The 'AbstractFactory' abstract class
     /// </summary>
--- Design_Patterns_CSharp/Design_Patterns_CSharp/Abstract_Factory/AbstractFactory_Structural.cs
+++ Design_Patterns_CSharp/Design_Patterns_CSharp/Abstract_Factory/AbstractFactory_Structural.cs
@@ -6,6 +6,25 @@ using System.Threading.Tasks;
 
 namespace Design_Patterns_CSharp.Abstract_Factory
 {
+    ///// <summary>
+    ///// Entry point into console application.
+    ///// </summary>
+    //public static void Main()
+    //{
+    //    // Abstract factory #1
+    //    AbstractFactory factory1 = new ConcreteFactory1();
+    //    Client client1 = new Client(factory1);
+    //    client1.Run();
+
+    //    // Abstract factory #2
+    //    AbstractFactory factory2 = new ConcreteFactory2();
+    //    Client client2 = new Client(factory2);
+    //    client2.Run();
+
+    //    // Wait for user input
+    //    Console.ReadKey();
+    //}
+
     /// <summary>
     /// The 'AbstractFactory' abstract class
     /// </summary>
