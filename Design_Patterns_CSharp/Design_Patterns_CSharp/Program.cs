4368e69181d83de679b12c96c782d1a536d7f4c6
adjutant-xx <matt.paauw@gmail.com>
2017-02-06 15:40:23 -0800

    Pushing initial project build with boilerplate code.

---
+++ Design_Patterns_CSharp/Design_Patterns_CSharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
