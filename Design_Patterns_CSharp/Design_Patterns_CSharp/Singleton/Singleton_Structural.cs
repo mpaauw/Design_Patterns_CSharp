1c5223c70b4fff8b2064db6edcd8f3b60fc0a8a1
adjutant-xx <matt.paauw@gmail.com>
2017-02-06 16:28:03 -0800

    Added code for Singleton pattern.

---
+++ Design_Patterns_CSharp/Design_Patterns_CSharp/Singleton/Singleton_Structural.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns_CSharp.Singleton
+{
+    class Singleton_Structural
+    {
+        ///// <summary>
+        ///// MainApp startup class for Structural
+        ///// Singleton Design Pattern.
+        ///// </summary>
+        //class MainApp
+        //{
+        //    /// <summary>
+        //    /// Entry point into console application.
+        //    /// </summary>
+        //    static void Main()
+        //    {
+        //        // Constructor is protected -- cannot use new
+        //        Singleton s1 = Singleton.Instance();
+        //        Singleton s2 = Singleton.Instance();
+
+        //        // Test for same instance
+        //        if (s1 == s2)
+        //        {
+        //            Console.WriteLine("Objects are the same instance");
+        //        }
+
+        //        // Wait for user
+        //        Console.ReadKey();
+        //    }
+        //}
+
+        /// <summary>
+        /// The 'Singleton' class
+        /// </summary>
+        class Singleton
+        {
+            private static Singleton _instance;
+
+            // Constructor is 'protected'
+            protected Singleton()
+            {
+            }
+
+            public static Singleton Instance()
+            {
+                // Uses lazy initialization.
+                // Note: this is not thread safe.
+                if (_instance == null)
+                {
+                    _instance = new Singleton();
+                }
+
+                return _instance;
+            }
+        }
+    }
+}
