1b9e8ea5fdc8d71f825b401a481c393b4f9af3d5
adjutant-xx <matt.paauw@gmail.com>
2017-02-09 18:50:02 -0800

    Added code for the Bridge pattern.

---
+++ Design_Patterns_CSharp/Design_Patterns_CSharp/Structural_Patterns/Bridge/Bridge_Structural.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Patterns_CSharp.Structural_Patterns.Bridge
+{
+    class Bridge_Structural
+    {
+        ///// <summary>
+        ///// MainApp startup class for Structural
+        ///// Bridge Design Pattern.
+        ///// </summary>
+        //class MainApp
+        //{
+        //    /// <summary>
+        //    /// Entry point into console application.
+        //    /// </summary>
+        //    static void Main()
+        //    {
+        //        Abstraction ab = new RefinedAbstraction();
+
+        //        // Set implementation and call
+        //        ab.Implementor = new ConcreteImplementorA();
+        //        ab.Operation();
+
+        //        // Change implemention and call
+        //        ab.Implementor = new ConcreteImplementorB();
+        //        ab.Operation();
+
+        //        // Wait for user
+        //        Console.ReadKey();
+        //    }
+        //}
+
+        /// <summary>
+        /// The 'Abstraction' class
+        /// </summary>
+        class Abstraction
+        {
+            protected Implementor implementor;
+
+            // Property
+            public Implementor Implementor
+            {
+                set { implementor = value; }
+            }
+
+            public virtual void Operation()
+            {
+                implementor.Operation();
+            }
+        }
+
+        /// <summary>
+        /// The 'Implementor' abstract class
+        /// </summary>
+        abstract class Implementor
+        {
+            public abstract void Operation();
+        }
+
+        /// <summary>
+        /// The 'RefinedAbstraction' class
+        /// </summary>
+        class RefinedAbstraction : Abstraction
+        {
+            public override void Operation()
+            {
+                implementor.Operation();
+            }
+        }
+
+        /// <summary>
+        /// The 'ConcreteImplementorA' class
+        /// </summary>
+        class ConcreteImplementorA : Implementor
+        {
+            public override void Operation()
+            {
+                Console.WriteLine("ConcreteImplementorA Operation");
+            }
+        }
+
+        /// <summary>
+        /// The 'ConcreteImplementorB' class
+        /// </summary>
+        class ConcreteImplementorB : Implementor
+        {
+            public override void Operation()
+            {
+                Console.WriteLine("ConcreteImplementorB Operation");
+            }
+        }
+    }
+}
